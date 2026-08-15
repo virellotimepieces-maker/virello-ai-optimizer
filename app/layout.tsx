8e4ebaa3ed26900f4278f93eb31800258630d161
virellotimepieces-maker <virellotimepieces@gmail.com>
2026-08-15 09:14:05 -0400

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,6 +13,13 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <meta
+          name="shopify-api-key"
+          content={process.env.SHOPIFY_API_KEY || ""}
+        />
+        <script src="https://cdn.shopify.com/shopifycloud/app-bridge.js"></script>
+      </head>
       <body>{children}</body>
     </html>
   );
